d27e7808d3cb7b060e3920c669379cd208b460ed
jiaoyaning <jiaoyaning@koolearn-inc.com>
2019-08-06 20:45:18 +0800

    update 添加测试

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 ##Gradle  
 Step 1. Add it in your root build.gradle at the end of repositories:
+
 ```
 allprojects {
 	repositories {
@@ -15,6 +16,7 @@ allprojects {
 }
 ```
 Step 2. Add the dependency 
+
 
 ```
  dependencies {
--- app/src/main/java/com/jyn/marqueeview/MainActivity.java
+++ app/src/main/java/com/jyn/marqueeview/MainActivity.java
@@ -3,10 +3,18 @@ package com.jyn.marqueeview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.jyn.marqueetextview.MarqueeView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+
+    private MarqueeView marqueeView;
+    private TextView speed_text, spacing_text;
+
+    private SeekBar speed_seekbar, spacing_seekbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +24,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        MarqueeView marqueeView = findViewById(R.id.marquee);
+        marqueeView = findViewById(R.id.marquee);
         marqueeView.setText("东风夜放花千树，更吹落星如雨，宝马雕车香满路，凤箫声动，玉壶光转，一夜鱼龙舞。");
+        speed_text = findViewById(R.id.speed_text);
+        spacing_text = findViewById(R.id.spacing_text);
 
-        MarqueeView marqueeView2 = findViewById(R.id.marquee2);
-        marqueeView2.setText("东风夜放花千树，更吹落星如雨，宝马雕车香满路，凤箫声动，玉壶光转，一夜鱼龙舞。");
+        speed_seekbar = findViewById(R.id.speed_seekbar);
+        spacing_seekbar = findViewById(R.id.spacing_seekbar);
+
+        speed_seekbar.setOnSeekBarChangeListener(this);
+        spacing_seekbar.setOnSeekBarChangeListener(this);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        if (seekBar == speed_seekbar) {
+            speed_text.setText("速度：" + i);
+            marqueeView.setSpeed(i);
+        } else if (seekBar == spacing_seekbar) {
+            speed_text.setText("间隔：" + i);
+            marqueeView.setSpacing(i);
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
